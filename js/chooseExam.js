4497d8c00617be9e6b82d377021ebecf957018c3
MostafaNegm222 <mostafanegm0002@gmail.com>
2025-01-26 16:47:17 +0200

    edit path

--- js/chooseExam.js
+++ js/chooseExam.js
@@ -1,16 +1,16 @@
 document.querySelector('.html').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'html')
-    window.location.replace('startExam.html')
+    window.location.replace('../pages/startExam.html')
 })
 document.querySelector('.css').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'css')
-    window.location.replace('startExam.html')
+    window.location.replace('../pages/startExam.html')
 })
 document.querySelector('.js').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'js')
-    window.location.replace('startExam.html')
+    window.location.replace('../pages/startExam.html')
 })
 document.querySelector('.logout').addEventListener('click' , function () {
     sessionStorage.clear()
-    window.location.replace("login.html")
+    window.location.replace("/login.html")
 })
