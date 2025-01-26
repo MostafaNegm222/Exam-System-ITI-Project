ae60136c752baf93e9e83681acfea0d85c41dbd4
MostafaNegm222 <mostafanegm0002@gmail.com>
2025-01-26 17:05:47 +0200

    edit

--- js/chooseExam.js
+++ js/chooseExam.js
@@ -1,14 +1,14 @@
 document.querySelector('.html').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'html')
-    window.location.replace('./startExam.html')
+    window.location.replace('./startexam.html')
 })
 document.querySelector('.css').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'css')
-    window.location.replace('./startExam.html')
+    window.location.replace('./startexam.html')
 })
 document.querySelector('.js').addEventListener('click' , function () {
     sessionStorage.setItem('category' , 'js')
-    window.location.replace('./startExam.html')
+    window.location.replace('./startexam.html')
 })
 document.querySelector('.logout').addEventListener('click' , function () {
     sessionStorage.clear()
